f5ac878f59a6cfc618c7cebedf2be9263bb326c4
weeee9 <teletubby332@gmail.com>
2020-09-04 23:42:18 +0800

    chore: rename file

--- problems/0026.Remove-Duplicates-From-Sorted-Array/remove_duplicates_test.go
+++ problems/0026.Remove-Duplicates-From-Sorted-Array/remove_duplicates_test.go
@@ -6,29 +6,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var testcase = []struct {
-	nums     []int
-	expected int
-}{
-	{
-		nums:     []int{1, 1, 2},
-		expected: 2,
-	},
-	{
-		nums:     []int{0, 0, 1, 1, 1, 1, 2, 3, 4, 4},
-		expected: 5,
-	},
-	{
-		nums:     []int{0, 0, 0, 0, 0},
-		expected: 1,
-	},
-	{
-		nums:     []int{1},
-		expected: 1,
-	},
-}
-
 func TestRemoveDuplicates(t *testing.T) {
+
+	var testcase = []struct {
+		nums     []int
+		expected int
+	}{
+		{
+			nums:     []int{1, 1, 2},
+			expected: 2,
+		},
+		{
+			nums:     []int{0, 0, 1, 1, 1, 1, 2, 3, 4, 4},
+			expected: 5,
+		},
+		{
+			nums:     []int{0, 0, 0, 0, 0},
+			expected: 1,
+		},
+		{
+			nums:     []int{1},
+			expected: 1,
+		},
+	}
+
 	t.Run("0026.Remov Duplicates from Sorted Array", func(t *testing.T) {
 		for _, test := range testcase {
 			got := removeDuplicates(test.nums)
